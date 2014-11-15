e65670872a16bf08c53bc634f718d8a4b0fc476e
manojgudi <manoj.p.gudi@gmail.com>
2014-11-15 18:38:16 +0530

    Fixed race condition, added command Line arguments (target files)

--- redo_example/redo.hs
+++ redo_example/redo.hs
@@ -1,5 +1,17 @@
-import System.Process
+import System.Environment (getArgs)
+import System.Process (shell, createProcess, waitForProcess)
 
+main :: IO ()
 main = do
-    _ <- createProcess $ shell "sh redo.do"
+    -- Take args
+    args <- getArgs
+    -- Apply redo to all filenames passed as arguements
+    mapM_ redo args
+
+redo :: String -> IO ()
+redo target = do
+    (_, _, _, pHandle) <- createProcess $ shell $ "sh " ++ target ++ ".do"
+    -- without waitForProcess the program exists, closing the shell process
+    -- spawned
+    _ <- waitForProcess pHandle
     return()
